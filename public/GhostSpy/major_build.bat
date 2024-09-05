bdc71b8b3bfeb7f8b65e3bc993fc83b5d3b6101c
AK500M <azmatkassayev0032@outlook.com>
2024-09-05 18:09:02 +0200

    update bats

--- public/GhostSpy/major_build.bat
+++ public/GhostSpy/major_build.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "F:\Development_Projects\TechDroidDevelopment\ghostspy\github\api\public\GhostSpy\GhostSpy_major"
+cd /d "C:\Users\Administrator\Downloads\Ghostspy\ghostspy-BE\public\GhostSpy\GhostSpy_major"
 
 REM Run the Gradle command to assemble the debug APK
 gradlew assembleDebug
