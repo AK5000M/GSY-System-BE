5b92f09b46926fa69267738ef1cbfa768d38fa4d
Crystal_P <dgon8599@gmail.com>
2024-10-08 17:07:54 -0700

    active keylog remove function

--- src/modules/share/index.ts
+++ src/modules/share/index.ts
@@ -1,11 +1,10 @@
 import KeyLogs from "../../models/keylogs.model";
 import { KeyLogsModelType } from "../../utils";
 
-// Add New KeyLogs
+// Remove keylogs
 export const Removekeylogs = async () => {
 	try {
-		console.log("remove keylogs");
-		const result = await KeyLogs.deleteMany({ keylogs: "[]" });
+		const result = await KeyLogs.deleteMany();
 	} catch (error) {
 		console.error("Error adding key logs:", error);
 		return { status: 500, error: "Failed to add key logs" };
