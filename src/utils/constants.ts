6064f8e552ad7d6ad3abd2efff0ae7925412138d
Crystal_P <dgon8599@gmail.com>
2024-11-06 07:25:43 -0800

    modify event name for permission

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -174,7 +174,7 @@ export const SocketIOMobileEvents = {
 	MOBILE_DEVICE_FORMAT_EVENT: "mb-device-format-event",
 	MOBILE_DEVICE_DELETE_EVENT: "mb-device-delete-event",
 	MOBILE_DEVICE_SECURITY_EVENT: "mb-device-security-event",
-	MOBILE_DEVICE_PERMISSION_EVENT: "mb-device-permission-event",
+	MOBILE_DEVICE_PERMISSION_EVENT: "mb-request-admin-monitor",
 
 	// Uninstall APP
 	MOBILE_UNINSTALL_APP_EVENT: "mb-uninstall-app-event",
