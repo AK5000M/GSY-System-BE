ba6846bd2830c55b547a086ed8169c444c4e70d7
Crystal_P <dgon8599@gmail.com>
2025-01-20 10:24:44 -0800

    modify app building and bat files

--- public/GhostSpy/major_build.bat
+++ public/GhostSpy/major_build.bat
@@ -3,4 +3,4 @@ cd /d "C:\Users\Administrator\Downloads\Ghostspy\ghostspy-BE\public\GhostSpy\Gho
 REM cd /d "F:\Development_Projects\TechDroidDevelopment\ghostspy\github\api\public\GhostSpy\GhostSpy_major"
 
 REM Run the Gradle command to assemble the debug APK
-gradlew assembleDebug
+gradlew assembleRelease
